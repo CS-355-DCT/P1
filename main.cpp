7d4be8154a7ed43de32c14d817b03a65bbd8b754
Machine517 <aclark15@una.edu>
2023-09-23 13:25:41 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// main.cpp
+
+#include <iostream>
+#include "scoreboard.h"
+
+int main() {
+
+Scoreboard scoreboard;
+
+    // Implement your menu-driven program here
+
+    // Prompt the user to add scores and display the total score
+
+    return 0;
+}
---
+++ scoreboard.cpp
@@ -0,0 +1,13 @@
+// scoreboard.cpp
+
+#include "scoreboard.h"
+
+Scoreboard::Scoreboard() : totalScore(0) {}
+
+void Scoreboard::addScore(const char* teamMember, int score) {
+// Implement adding score here
+}
+
+int Scoreboard::getTotalScore() const {
+    return totalScore;
+}
