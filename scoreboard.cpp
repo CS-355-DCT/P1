9d0a3214c7739134ef8a51445f1e08a6058a6de6
Thomas Bendall <thbend@cs.csis.work>
2023-09-25 14:20:27 -0500

    addScore() implementation

--- scoreboard.cpp
+++ scoreboard.cpp
@@ -1,11 +1,14 @@
 // scoreboard.cpp
 
 #include "scoreboard.h"
+#include <iostream>
 
 Scoreboard::Scoreboard() : totalScore(0) {}
 
 void Scoreboard::addScore(const char* teamMember, int score) {
 // Implement adding score here
+	totalScore += score;
+	cout << *teamMember << " has scored " << score << " points" << endl;
 }
 
 int Scoreboard::getTotalScore() const {
